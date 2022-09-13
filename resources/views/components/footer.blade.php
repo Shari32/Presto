1b4bab291abcc1280d713a7041ba1c641e63f54c
Alessandro La Piana <alessandrolapiana@outlook.com>
2022-09-13 12:16:05 +0200

    da fixare upload immagine

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -51,7 +51,7 @@
 						<!-- Content -->
 						<h6 class="text-uppercase fw-bold">Team Panda</h6>
 						<hr class="mb-4 mt-0 d-inline-block mx-auto hr-footer">
-						<p> Presto.it, per vendere le tue cianfrusaglie presto.</p>
+						<p> Presto.it, per vendere le tue cianfrusaglie al piu' presto.</p>
 					</div>
 
 					<!-- Grid column -->
@@ -63,7 +63,7 @@
 						<p><a href="#!" class="text-white">Automobili</a></p>
 						<p><a href="#!" class="text-white">Elettronica</a></p>
 						<p><a href="#!" class="text-white">Immobili</a></p>
-						<p><a href="#!" class="text-white">Film e TV</a></p>
+						{{-- <p><a href="#!" class="text-white">Film e TV</a></p> --}}
 						
 					</div>
 
@@ -75,8 +75,8 @@
 						<hr class="mb-4 mt-0 d-inline-block mx-auto hr-footer">
 						<p><a href="#!" class="text-white">Account</a></p>
 						<p><a href="{{route('become_revisor')}}" class="text-white">Diventa un revisore</a></p>
-						<p><a href="#!" class="text-white">Non so cosa scrivere</a></p>
-						<p><a href="#!" class="text-white">Help</a></p>
+						<p><a href="#!" class="text-white">Categorie</a></p>
+						{{-- <p><a href="#!" class="text-white">Contatti</a></p> --}}
 					
 					</div>
 
@@ -87,10 +87,10 @@
 						<!-- Links -->
 						<h6 class="text-uppercase fw-bold">Contatti:</h6>
 						<hr class="mb-4 mt-0 d-inline-block mx-auto hr-footer">
-						<p><i class="fas fa-home mr-3"></i> Aulab, Strada S. Giorgio Martire 2D, 70124 Bari</p>
+						<p><i class="fas fa-home mr-3"></i> Aulab, Strada S. Giorgio M. 2D, Bari</p>
 						<p><i class="fas fa-envelope mr-3"></i> Presto.it </p>
 						<p><i class="fas fa-phone mr-3"></i> Inserire Numero </p>
-						<p><i class="fas fa-print mr-3"></i> Inserire Fax</p>
+						{{-- <p><i class="fas fa-print mr-3"></i> Inserire Fax</p> --}}
 					
 					</div>
 
