0dbc3bedf670f1d434dc272bff8bb315581b2fa9
Alessandro La Piana <alessandrolapiana@outlook.com>
2022-09-14 19:09:38 +0200

    user story 8 conclusa

--- app/Http/Livewire/AdCreateForm.php
+++ app/Http/Livewire/AdCreateForm.php
@@ -9,6 +9,7 @@ use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
@@ -87,10 +88,11 @@ class AdCreateForm extends Component
                 $newFileName = "ads/{$this->ad->id}";
                 $newImage = $this->ad->images()->create(['path'=>$image->store($newFileName,'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 400, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
-
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 400, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
 
             }
 
